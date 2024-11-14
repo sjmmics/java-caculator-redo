febd93d177daba5e4fb630b41d0cb626746f9525
Seo Jinmann <sjmmics@gmail.com>
2024-11-14 16:10:54 +0900

    feat : ($DefaultDelimiter) 기본 구분자 클래스 추가
    기본 구분자를 String으로 보관하며 정규식 패턴으로 반환하거나 char로 반환하는 기능을 갖는다.

--- src/main/java/calculator/model/DefaultDelimiter.java
+++ src/main/java/calculator/model/DefaultDelimiter.java
@@ -1,4 +1,17 @@
 package calculator.model;
 
-public class DefaultDelimiter {
+import java.util.regex.Pattern;
+
+public record DefaultDelimiter(String letter) implements Delimiter {
+    
+    @Override
+    public String getPatternQuote() {
+        return Pattern.quote(letter);
+    }
+    
+    @Override
+    public char getChar() {
+        return letter.charAt(0);
+    }
+    
 }
