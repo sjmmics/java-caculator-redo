9f6d9c55f0ac6607c07249f1227193bd4ce17215
Seo Jinmann <sjmmics@gmail.com>
2024-11-14 16:22:01 +0900

    feat : ($Delimiters) 구분자들을 담는 클래스
    구분자들을 보관한다.
    
    구분자로 문자열을 분리할 때 쓰는 정규식 패턴을 반환하는 기능, 문자열에 있는 문자가 숫자, 구분자인지 확인하는 기능을 갖는다.

--- src/main/java/calculator/model/Delimiters.java
+++ src/main/java/calculator/model/Delimiters.java
@@ -1,4 +1,54 @@
 package calculator.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Delimiters {
+    
+    private final List<Delimiter> delimiters;
+    
+    public Delimiters(List<Delimiter> delimiters) {
+        this.delimiters = delimiters;
+    }
+    
+    public static Delimiters getInitialize() {
+        List<Delimiter> delimiters = new ArrayList<>();
+        Delimiter comma = new DefaultDelimiter(",");
+        Delimiter semiColon = new DefaultDelimiter(":");
+        delimiters.add(comma);
+        delimiters.add(semiColon);
+        return new Delimiters(delimiters);
+    }
+    
+    public void addCustomDelimiter(CustomDelimiter customDelimiter) {
+        String letter = customDelimiter.letter();
+        if (!letter.isEmpty()) {
+            delimiters.add(customDelimiter);
+        }
+    }
+    
+    public String getSplitRegex() {
+        return delimiters.stream()
+                .map(Delimiter::getPatternQuote)
+                .collect(Collectors.joining(")|(", "(", ")"));
+    }
+    
+    public boolean isProperLetter(char letter) {
+        if (Character.isDigit(letter)) {
+            return true;
+        }
+        return containsCharacter(letter);
+    }
+    
+    private boolean containsCharacter(char letter) {
+        for (Delimiter delimiter : delimiters) {
+            if (letter == delimiter.getChar()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
