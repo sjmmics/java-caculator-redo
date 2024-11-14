ca7086f0d42057d4a13da1f4003e440d468ff2fe
Seo Jinmann <sjmmics@gmail.com>
2024-11-14 16:08:58 +0900

    feat : ($InputView) 입력 처리 및 입력 안내 출력 클래스 추가
    사용자한테 입력 안내 메시지를 출력하고 입력을 받아서 controller에 전달한다.

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,4 +1,12 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    
+    public String getLine() {
+        System.out.println(IOMessage.GUIDE_INPUT);
+        return Console.readLine();
+    }
+    
 }
