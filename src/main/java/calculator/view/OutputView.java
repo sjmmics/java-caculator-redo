f9ceeaa96c60165d6c9d1347d9ff533a757db61e
Seo Jinmann <sjmmics@gmail.com>
2024-11-14 16:30:30 +0900

    feat : ($OutputView) 결과값 출력을 담당하는 클래스 추가
    controller에서 요청을 받아서 구분자로 분리한 문자열의 수의 합을 출력하는 기능을 담당한다.

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,4 +1,10 @@
 package calculator.view;
 
+import calculator.model.Result;
+
 public class OutputView {
+    public void printResult(Result result) {
+        System.out.print(IOMessage.SHOW_RESULT_PREFIX);
+        System.out.println(result);
+    }
 }
